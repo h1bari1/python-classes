65348779cfed3b18786c186df92931a57f846d5a
Roman Kremez <vhibari1@gmail.com>
2024-09-21 11:04:01 +0300

    init studet project

---
+++ main.py
@@ -0,0 +1,63 @@
+from random import *
+
+
+class Student:
+
+    def __init__(self, name):
+        self.name = name
+        self.gladness = 50
+        self.progress = 0
+        self.alive = True
+
+    def to_study(self):
+        print("I`m study!")
+        self.progress += 0.12
+        self.gladness -= 5
+
+    def to_sleep(self):
+        print("I will sleep")
+        self.gladness += 3
+
+    def to_chill(self):
+        print("Time to rest")
+        self.gladness += 5
+        self.progress -= 0.1
+
+    def is_alive(self):
+        if self.progress < -0.5:
+            print("Cast out...")
+            self.alive = False
+        elif self.gladness <= 0:
+            print("Depression...")
+            self.alive = False
+        elif self.progress > 5:
+            print("Passed...")
+            self.alive = False
+
+    def end_of_day(self):
+        print(f"Gladness = {self.gladness}")
+        print(f"Progress = {round(self.progress, 2)}")
+
+    def live(self, day):
+        day = "Day " + str(day + 1) + " of " + self.name + " life."
+        print(f"{day:^50}")
+        live_cube = randint(1, 3)
+
+        if live_cube == 1:
+            self.to_study()
+        elif live_cube == 2:
+            self.to_sleep()
+        elif live_cube == 3:
+            self.to_chill()
+
+        self.end_of_day()
+        self.is_alive()
+
+
+vasya = Student(name="Vasya")
+
+for day in range(365):
+    if not vasya.alive:
+        break
+
+    vasya.live(day)
